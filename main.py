dcda1bd735cccaee59e8013a999ed1987f87fa8e
Michael-Macke <michael.c.macke@gmail.com>
2021-02-24 11:20:50 -0600

    Adding main to repo

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 24 11:04:56 2021
+
+@author: michael
+"""
+
+
+"""This is the main file for the market analysis project
+       The main aim of this project is the practice and learn more aspects of coding
+       and create a tool for my own personal use"""
+       
+
+       
+
