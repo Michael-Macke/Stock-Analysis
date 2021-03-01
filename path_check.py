0f04316d2827a5ca8e8e612b0281e38e5cb35b54
Michael-Macke <michael.c.macke@gmail.com>
2021-03-01 09:08:02 -0600

    Add path_checking function file

---
+++ path_check.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  1 09:01:07 2021
+
+@author: michael
+"""
+
+"""This function checks if the folder/file exsists or not and returns true/false
+   accordingly"""
+   
+def path_check(to_check):
+    
