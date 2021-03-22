20bacd81bfc6f2ec5c7bbe23bf04f312d89b9837
Michael-Macke <michael.c.macke@gmail.com>
2021-03-22 08:40:35 -0500

    Data visualization automation working

--- data_vis.py
+++ data_vis.py
@@ -10,15 +10,24 @@ from read_sym import read_sym
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 """This file wraps up the functionality to create visualizations of the data
       using date as the X axis and each of the columns individually as the
-      Y axes"""
+      Y axes
+   For visualization we utilize pythons seaborn library to create the lineplots
+   CURRENT TASK:
+       Find how to expand the size of the figure created so that it is screen wide"""
+      
       
 #def data_vis():
 list_list = ["Companies", "Commodities"]
 Corpsym = read_sym("Companies")
 Comsym  = read_sym("Commodities")
+
+BA_data = pd.read_csv('/home/michael/Documents/Companies/BA_data/BA.csv', index_col = 'formatted_date')
+BA_data.index = pd.to_datetime(BA_data.index, format = '%Y-%m-%d')
+sns.lineplot(data = BA_data["high"])
 
 for sect in list_list:
     Symlist = read_sym(sect)
@@ -28,27 +37,16 @@ for sect in list_list:
         CorpPath = os.path.join(DocRoot, Corp)
         #data_modernize updates the data to the current dates data
         print("Updating data for:", symbol)
-        data = pd.read_csv(CorpPath)
+        data = pd.read_csv(CorpPath, index_col = 'formatted_date')
+        data.index = pd.to_datetime(data.index, format = "")
         col_list = list(data.columns.values.tolist())
-        col_list.remove('formatted_date')
         for column in col_list:
-            fig = plt.figure(figsize=(10,6))
-            
-            ax1 = fig.add_axes([0,0, 1, 1])
-            
-            title = symbol + "_" + column
-            
-            ax1.set_title(title)
-            
-            ax1.plot(data['formatted_date'], data[column], color = "blue")
-            
-            png_path = sect + "/" + symbol + "_data/" + title + ".png"
-            
-            pngfinal = os.path.join(DocRoot, png_path)
-            
-            plt.show()
-            
-            plt.savefig(pngfinal, bbox_inches = "tight", pad_inches = 1.1)
+            col_path = sect + "/" + symbol + "_data/" + symbol + "_"+ column + ".jpg"
+            png_path = os.path.join(DocRoot, col_path)
+            sns.lineplot(data = data[column])
+            #plt.show()
+            plt.savefig(png_path)
+            plt.clf()
             
     
     #BA_data = pd.read_csv('/home/michael/Documents/Companies/BA_data/BA.csv')
